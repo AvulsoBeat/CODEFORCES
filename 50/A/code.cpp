cc167172e34ffde303333bb86b17f265264eb911
AvulsoBeat <guilhermeap330@gmail.com>
2021-05-23 14:15:56 -0300

    torneio 50 A pt2

--- 50/A/code.cpp
+++ 50/A/code.cpp
@@ -7,30 +7,7 @@ int main() {
     cin >> m;
     cin >> n;
 
-    int a[m][n];
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            a[i][j] = 0;
-        }
-    }
-
-    int domino = 0;
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++){
-            // checa na direita
-            if(a[i][j] == 0 && j+1 < n && a[i][j+1] == 0) {
-                domino++;
-                a[i][j] = 1;
-                a[i][j+1] = 1;
-            }
-            // checa em baixo
-            if (a[i][j] == 0 && i+1 < m && a[i+1][j] == 0){
-                domino++;
-                a[i][j] = 1;
-                a[i+1][j] = 1;
-            }
-        }
-    }
+    int domino = m * n / 2;
 
     cout << domino << endl;
     return 0;
